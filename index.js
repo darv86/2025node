f1f824a9ffbb7f79704726b6333dbafaa032058e
Dima R <86darv@gmail.com>
2025-01-13 11:16:56 +0200

    buffers

--- index.js
+++ index.js
@@ -1,137 +1,69 @@
-const bin1 = 1 * 2 ** 0;
-const bin2 = 1 * 2 ** 1;
-const bin3 = 1 * 2 ** 2;
-const bin4 = 0 * 2 ** 3;
-const binResult = bin1 + bin2 + bin3 + bin4;
-// console.log(binResult, '\n', bin1, bin2, bin3, bin4);
+import { Buffer, constants } from 'node:buffer';
 
-const dec1 = 8 * 10 ** 0;
-const dec2 = 5 * 10 ** 1;
-const dec3 = 3 * 10 ** 2;
-const decResult = dec1 + dec2 + dec3;
-// console.log(decResult, '\n', dec1, dec2, dec3);
+// the buffer object has a lot of methods as an array has,
+// coz the Buffer class is inherited from the Uint8Array class
 
-const HEX2BIN = {
-	0: '0000',
-	1: '0001',
-	2: '0010',
-	3: '0011',
-	4: '0100',
-	5: '0101',
-	6: '0110',
-	7: '0111',
-	8: '1000',
-	9: '1001',
-	a: '1010',
-	b: '1011',
-	c: '1100',
-	d: '1101',
-	e: '1110',
-	f: '1111',
-};
+// two constants that show how big memory can be allocated to this buffer
+// (theoretical maximum amount of a memory for this OS)
+// console.log(constants.MAX_LENGTH);
 
-const HEX2DEC = {
-	0: 0,
-	1: 1,
-	2: 2,
-	3: 3,
-	4: 4,
-	5: 5,
-	6: 6,
-	7: 7,
-	8: 8,
-	9: 9,
-	a: 10,
-	b: 11,
-	c: 12,
-	d: 13,
-	e: 14,
-	f: 15,
-};
+// the buffer is an array like data structure, but not an array
+// the min portion of a buffer is one byte (in this case are 4 bytes)
+// or 2 nibbles (4 bits + 4 bits)
+const memory = Buffer.alloc(4, 5);
+// shows the buffer (memory) content, using hex representation
+// console.log(memory);
 
-const hex1 = HEX2DEC['c'] * 16 ** 0;
-const hex2 = HEX2DEC['d'] * 16 ** 1;
-const hex3 = HEX2DEC[5] * 16 ** 2;
-const hexResult = hex1 + hex2 + hex3;
-// console.log({ hexResult }, '\n', hex1, hex2, hex3);
+// can write to the specific byte (1)
+memory[1] = 2;
+// proper way to write and read numbers from buffer
+// are two groups of a methods: write... read...
+memory.writeInt8(-3, 1);
+// console.log(memory.readInt8(1));
+// if there is need to fill every byte of the whole buffer with the specific value,
+// use this 'fill' method - fast approach
+memory.fill(5);
 
-/**
- * @param {string} hexNum
- * @returns {number}
- */
-function hexToDec(hexNum) {
-	const data = hexNum.startsWith('0x') ? hexNum.slice(2) : hexNum;
-	return data
-		.toLowerCase()
-		.split('')
-		.reverse()
-		.reduce((result, symbol, i) => result + HEX2DEC[symbol] * 16 ** i, 0);
-}
-const resHexToDec = hexToDec('0xfa3c');
-// console.log(resHexToDec);
+// TASK 1
+const data = '0100 1000 0110 1001 0010 0001'.replaceAll(' ', '');
+const BUFFER_SIZE = data.length / 8;
+const bytes = data
+	.split('')
+	.reduce(
+		(result, _, i, arr) => (i % 8 === 0 && result.push(arr.slice(i, i + 8).join('')), result),
+		/** @type {string[]}*/ ([])
+	);
+const binary = bytes.reduce(
+	(result, _, i, arr) => (result.push(parseInt(arr[i], 2)), result),
+	/** @type {number[]}*/ ([])
+);
+// creates buffer with the specific size
+const buff = Buffer.alloc(BUFFER_SIZE);
+// and then fulfill this buffer with a data
+buff.forEach((_, i, arr) => arr.writeInt8(binary[i], i));
+// here we can creates buffer and fulfill with binary (bytes with decimals or hex values) array
+const buffEz = Buffer.from(binary);
+// console.log(buffEz.toString('utf-8')); // Hi!
+// can even pass string of hex ('486921'),
+// but should specify second parameter 'hex' for the 'from' method
+const binaryHex = binary.map(byte => byte.toString(16));
+const buffHex = Buffer.from(binaryHex.join(''), 'hex');
+// console.log(buffHex.toString('utf-8')); // Hi!
+// or simply create a buffer from a string as a value
+// console.log(Buffer.from('my string', 'utf-8').toString());
 
-/**
- * @param {string} binary
- * @returns {number}
- */
-function binaryToDec(binary) {
-	return binary
-		.split('')
-		.reverse()
-		.reduce((result, symbol, i) => result + Number(symbol) * 2 ** i, 0);
-}
-const resBinaryToDec = binaryToDec('0111');
-// console.log(resBinaryToDec);
+// content of the buffers can be compared
+const isEqual = buffEz.equals(buffHex);
+// console.log(isEqual);
 
-/**
- * @param {string} binary
- * @returns {string}
- */
-function binaryToHex(binary) {
-	let chunk = [];
-	let CHUNK_SIZE = 4;
-	const bytes = binary.split('').reduce((result, byte) => {
-		chunk.push(byte);
-		if (chunk.length === CHUNK_SIZE) {
-			result.push(chunk.join(''));
-			chunk = [];
-			return result;
-		}
-		return result;
-	}, /**@type {string[]}*/ ([]));
-	const ints = bytes.map(byte => binaryToDec(byte));
-	const hexes = ints.map(int => Object.keys(HEX2DEC).find(symbol => HEX2DEC[symbol] === int));
-	return '0x' + hexes.join('');
-	// alternative realization
-	// return '0x' + binaryToDec(binary).toString(16);
-}
-const resBinaryToHex = binaryToHex('010111011100'); // 1500
-// console.log({ resBinaryToHex });
+// check if the buffer includes specific value among all bytes
+const isThere = buffHex.includes(0x48);
+console.log(isThere);
 
-const objKey = {
-	/**
-	 * @param {Object} obj
-	 * @param {any} value
-	 * @returns {unknown}
-	 */
-	where(obj, value) {
-		return Object.keys(obj).find(key => obj[key] === value);
-	},
-};
+// concatenate all buffers in array and returns buffer
+const concatBuf = Buffer.concat([buff, buffEz]);
 
-const num = 1500;
-/**
- * @param {number} num
- * @returns {string}
- */
-function decToHex(num) {
-	if (num === 0) {
-		return '0x';
-	}
-	const nextNum = Math.floor(num / 16);
-	const hexNum = objKey.where(HEX2DEC, num % 16);
-
-	return decToHex(nextNum) + hexNum;
-}
-const resDecToHex = decToHex(num);
-console.log({ resDecToHex }); // Expected output: '0x5dc'
+// allocUnsafe method is the faster way to allocate a memory, but security unsafe
+// concat and from methods use allocUnsafe under the hood,
+// but instantly fulfills with data, so these two methods are safe
+const unsafeBuf = Buffer.allocUnsafe(10_000);
